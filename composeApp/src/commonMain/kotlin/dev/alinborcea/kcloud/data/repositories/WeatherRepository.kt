9605249aeb2b0fc30ad37cf3dd45c37c743ae9b9
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2026-05-11 12:28:33 +0300

    added method

--- composeApp/src/commonMain/kotlin/dev/alinborcea/kcloud/data/repositories/WeatherRepository.kt
+++ composeApp/src/commonMain/kotlin/dev/alinborcea/kcloud/data/repositories/WeatherRepository.kt
@@ -4,4 +4,6 @@ import dev.alinborcea.kcloud.domain.models.WeatherResponse
 
 interface WeatherRepository {
     suspend fun getWeatherAt(location: String): WeatherResponse
+
+    suspend fun getWeatherForecast(location: String, days: Int): WeatherResponse
 }
